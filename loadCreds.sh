c591f18a2c740e465cfe4921648fb61dd65264c4
adar <adarenglert@gmail.com>
2020-04-25 16:20:42 +0300

    load credentials script added to repo

---
+++ loadCreds.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+aws configure set aws_access_key_id AKIA3W3ZEDH6VT4MVK6E
+aws configure set aws_secret_access_key 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
+aws configure set default.region us-east-1
+aws configure list
